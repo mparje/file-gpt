8a0c27164b060786c4e75a3578da3fc61f43a721
mparje <126131806+mparje@users.noreply.github.com>
2023-05-21 14:34:28 -0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -95,78 +95,60 @@ def text_to_docs(text: str) -> List[Document]:
         text_splitter = RecursiveCharacterTextSplitter(
             chunk_size=800,
             separators=["\n\n", "\n", ".", "!", "?", ",", " ", ""],
-            chunk_overlap=0,
+            max_chunks=None,
         )
-        chunks = text_splitter.split_text(doc.page_content)
-        for i, chunk in enumerate(chunks):
-            doc = Document(
-                page_content=chunk, metadata={"page": doc.metadata["page"], "chunk": i}
-            )
-            # Add sources a metadata
-            doc.metadata["source"] = f"{doc.metadata['page']}-{doc.metadata['chunk']}"
-            doc_chunks.append(doc)
+        chunks = text_splitter.split(doc)
+        doc_chunks.extend(chunks)
+
     return doc_chunks
 
 
-@st.cache(allow_output_mutation=True, show_spinner=False)
-def embed_docs(docs: List[Document]) -> VectorStore:
-    """Embeds a list of Documents and returns a FAISS index"""
-
-    if not st.session_state.get("OPENAI_API_KEY"):
-        raise AuthenticationError(
-            "Enter your OpenAI API key in the sidebar. You can get a key at https://platform.openai.com/account/api-keys."
-        )
-    else:
-        # Embed the chunks
-        embeddings = OpenAIEmbeddings(openai_api_key=st.session_state.get("OPENAI_API_KEY"))  # type: ignore
-        index = FAISS.from_documents(docs, embeddings)
-
-        return index
+@st.experimental_memo()
+def embed_text(text: str, model: Any) -> List[List[float]]:
+    """Embeds a string or list of strings using the specified model."""
+    if isinstance(text, str):
+        # Take a single string as one page
+        text = [text]
+    embeddings = model.embed_documents(text)
+    return embeddings
 
 
-@st.cache(allow_output_mutation=True)
-def search_docs(index: VectorStore, query: str) -> List[Document]:
-    """Searches a FAISS index for similar chunks to the query
-    and returns a list of Documents."""
-    # Search for similar chunks
-    docs = index.similarity_search(query, k=5)
-    return docs
+def wrap_text_in_html(text: str) -> str:
+    """Wrap text in HTML <pre> tag for rendering as monospace."""
+    return f"<pre>{text}</pre>"
 
 
-@st.cache(allow_output_mutation=True)
-def get_answer(docs: List[Document], query: str) -> Dict[str, Any]:
-    """Gets an answer to a question from a list of Documents."""
+def main():
+    st.title("Language Chain Demo")
+    st.markdown(STUFF_PROMPT)
 
-    # Get the answer
+    file = st.file_uploader("Upload a file", type=["txt", "pdf", "docx", "csv"])
 
-    chain = load_qa_with_sources_chain(OpenAI(temperature=0, openai_api_key=st.session_state.get("OPENAI_API_KEY")), chain_type="stuff", prompt=STUFF_PROMPT)  # type: ignore
+    if file is not None:
+        if file.type == "application/pdf":
+            texts = parse_pdf(file)
+        elif file.type == "text/plain":
+            texts = [parse_txt(file)]
+        elif file.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+            texts = [parse_docx(file)]
+        elif file.type == "text/csv":
+            texts = [parse_csv(file)]
+        else:
+            texts = [parse_any(file)]
 
-    # Cohere doesn't work very well as of now.
-    # chain = load_qa_with_sources_chain(Cohere(temperature=0), chain_type="stuff", prompt=STUFF_PROMPT)  # type: ignore
-    answer = chain(
-        {"input_documents": docs, "question": query}, return_only_outputs=True
-    )
-    return answer
+        docs = text_to_docs(texts)
+        st.markdown(f"**Uploaded Text:**\n\n{wrap_text_in_html(docs[0].content)}")
+
+        # Initialize the embedding model
+        embedding_model = OpenAIEmbeddings(openai_api_key="your-openai-api-key")
+
+        # Embed the documents
+        embeddings = embed_text(docs, embedding_model)
+
+        # Perform downstream tasks using the embeddings
+        # ...
+        # ...
 
 
-@st.cache(allow_output_mutation=True)
-def get_sources(answer: Dict[str, Any], docs: List[Document]) -> List[Document]:
-    """Gets the source documents for an answer."""
-
-    # Get sources for the answer
-    source_keys = [s for s in answer["output_text"].split("SOURCES: ")[-1].split(", ")]
-
-    source_docs = []
-    for doc in docs:
-        if doc.metadata["source"] in source_keys:
-            source_docs.append(doc)
-
-    return source_docs
-
-
-def wrap_text_in_html(text: str | List[str]) -> str:
-    """Wraps each text block separated by newlines in <p> tags"""
-    if isinstance(text, list):
-        # Add horizontal rules between pages
-        text = "\n<hr/>\n".join(text)
-    return "".join([f"<p>{line}</p>" for line in text.split("\n")])
+if __name__ == "__main__":
+    main()
